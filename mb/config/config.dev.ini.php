5425e1a437b4ae2e38e770e782ec4c1a08856d79
gaoerxi <gaoerxi@000861.com>
2017-05-12 18:37:31 +0800

    1

--- mb/config/config.dev.ini.php
+++ mb/config/config.dev.ini.php
@@ -3,5 +3,5 @@ defined('InShopNC') or exit('Access Invalid!');
 global $config;
 $config['wechat']['token'] = 'comecome';
 $config['wechat']['encodingaeskey'] = 'aaaa';
-$config['wechat']['appid'] = 'bbbb';
-$config['wechat']['appsecret'] = 'cccc';
+$config['wechat']['appid'] = 'wx6b9b607c83b6be40';
+$config['wechat']['appsecret'] = 'a1fdbf0288f7b0cd0cfc5b27d8b7675b';
--- mb/control/wxresponse.php
+++ mb/control/wxresponse.php
@@ -5,16 +5,8 @@ defined ( 'InShopNC' ) or exit ( 'Access Invalid!' );
  * ******************************** 前台control父类 *********************************************
  */
 class wxresponseControl extends wechatClass {
-	private $options = array (
-			'token' => 'tokenaccesskey', // 填写你设定的key
-			'encodingaeskey' => 'encodingaeskey', // 填写加密用的EncodingAESKey
-			'appid' => 'wxdk1234567890', // 填写高级调用功能的app id
-			'appsecret' => 'xxxxxxxxxxxxxxxxxxx'  //填写高级调用功能的密钥
- 		);
-
 	public function __construct() {		
 		global $config;
-		var_dump($config);
 		parent::__construct ( $config['wechat'] );
 		$this->valid ();
 	}
